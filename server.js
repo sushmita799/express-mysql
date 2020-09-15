4a3d0d4a87527f98c81daa45046b097fec2b510d
Narendra Kurapati <nkurapati@salesforce.com>
2020-09-15 16:47:02 +0530

    Update Allow header header

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, content-type, X-Auth-Token');
     return next();
 });
 // simple route
