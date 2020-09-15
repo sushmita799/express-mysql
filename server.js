dbd62f91cf692074ac334eefa728fa543c4d2991
Narendra Kurapati <nkurapati@salesforce.com>
2020-09-15 16:34:08 +0530

    Update response headers

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     return next();
 });
 // simple route
